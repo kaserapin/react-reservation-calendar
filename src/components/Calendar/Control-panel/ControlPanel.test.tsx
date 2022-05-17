97c2236b1a13b99e2c96163209cca01c75e2dbdb
karolis.serapinas <karolis.serapinas@interactio.io>
2022-05-17 09:50:20 +0300

    fixed control panel tests

--- src/components/Calendar/Control-panel/ControlPanel.test.tsx
+++ src/components/Calendar/Control-panel/ControlPanel.test.tsx
@@ -12,27 +12,27 @@ describe('Control panel component', () => {
 
     const root = controlPanel.root;
 
-    it('should have class "calendar-control-panel"', () => {
-        const isClassNameCalendarControlPanel = root.findAllByType("div")[0].props.className.includes("calendar-control-panel");
+    it('should have class "calendarControlPanel"', () => {
+        const isClassNameCalendarControlPanel = root.findAllByType("div")[0].props.className.includes("calendarControlPanel");
         expect(isClassNameCalendarControlPanel).toBe(true);
     });
 
     it('should have "keyboard_arrow_left" icon name in leftArrow container', () => {
-        const leftArrowContainer = root.findByProps({className: "arrow-left"});
+        const leftArrowContainer = root.findByProps({className: "arrowLeft"});
         const iconContainer = leftArrowContainer.props.children.props.children;
 
         expect(iconContainer.includes("keyboard_arrow_left")).toBe(true);
     });
 
     it('should have "keyboard_arrow_right" icon name in rightArrow container', () => {
-        const rightArrowContainer = root.findByProps({className: "arrow-right"});
+        const rightArrowContainer = root.findByProps({className: "arrowRight"});
         const iconContainer = rightArrowContainer.props.children.props.children;
 
         expect(iconContainer.includes("keyboard_arrow_right")).toBe(true);
     });
 
     it('should call props.onLeftArrowClick when the left arrow is clicked', () => {
-        const leftArrowButton = root.findByProps({className: "arrow-left"});
+        const leftArrowButton = root.findByProps({className: "arrowLeft"});
 
         leftArrowButton.props.children.props.onClick();
 
@@ -40,7 +40,7 @@ describe('Control panel component', () => {
     });
 
     it('should call props.onRightArrowClick when the right arrow is clicked', () => {
-        const rightArrowButton = root.findByProps({className: "arrow-right"});
+        const rightArrowButton = root.findByProps({className: "arrowRight"});
 
         rightArrowButton.props.children.props.onClick();
 
